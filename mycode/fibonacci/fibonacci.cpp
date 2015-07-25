bfa1532a65a66ad6787f404c629b03878378a651
Vad on Lenovo E530 <vaddanak@gmail.com>
2015-07-25 03:13:13 -0400

    Finished fibonacci.py and fibonacci.java

---
+++ mycode/fibonacci/fibonacci.cpp
@@ -0,0 +1,6 @@
+/*
+Author: Vaddanak Seng
+File: fibonacci.cpp
+Purpose: Print out the first N numbers in the Fibonacci sequence.
+Date: 2015/07/25
+*/
---
+++ mycode/fibonacci/fibonacci.h
@@ -0,0 +1,10 @@
+/*
+Author: Vaddanak Seng
+File: fibonacci.h
+Purpose: Print out the first N numbers in the Fibonacci sequence.
+Date: 2015/07/25
+*/
+
+
+
+
