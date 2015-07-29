866422edaf0a86d9bfb498305e62083751b7891a
Vad on gateway laptop <vaddanak@gmail.com>
2015-07-29 10:05:09 -0400

    more edit

--- mycode/learning/cambridge.py
+++ mycode/learning/cambridge.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
 
+from __future__ import print_function;
+
+import sys;
+
+def test1():
+	print('coffee\ncaf' + unichr(0x00e8) + '\ncaffe' + 
+		unichr(0x00e9) + '\nKaffee\n', end='');
+	sys.stdout.write('How much? ');	
+	_input = sys.stdin.readline();
+	sys.stdout.write(str(float(_input) + 2.5));
+	
+
+
+
+test = test1;
 
 
 
 
+
+test();
