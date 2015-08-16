241c400e5fc1f1e7baa73f3b51198a1f925041c9
Vad on gateway laptop <vaddanak@gmail.com>
2015-08-16 17:09:13 -0400

    lots of work

--- mycode/learning/os_access.py
+++ mycode/learning/os_access.py
@@ -7,8 +7,8 @@ from __future__ import absolute_import;
 
 import sys, re;
 version = sys.version;
-
-###
+### '3.4.0 (default, Jun 19 2015, 14:20:21) \n[GCC 4.8.2]'
+### '2.7.6 (default, Jun 22 2015, 17:58:13) \n[GCC 4.8.2]'
 patternObject = re.compile(r'^(?P<major>\d+)\.(?P<minor>\d+)\.\d+\s?.*?');
 matchObject = patternObject.search(version);
 print('{:s}\n{:s}'.format(repr(version), matchObject.group(0)) );
@@ -42,9 +42,32 @@ else:
 	sys.exit(1);
 	
 ###
+
+
+import argparse;
+
+description_text = '''This is the description text that is fed to the 
+constructor when the parser is created.  It will be automatically formatted,
+so do not attempt to make clever format.'''
+parser = argparse.ArgumentParser(description=description_text);
+parser.add_argument('-o','--output', help='Name of output file', 
+	dest='output_file', default='output.dat', type=str);
+parser.add_argument(help='Directories to be processed', dest='directories',
+	nargs='*', default=[], type=str);
+arguments = parser.parse_args();
+
+
+# python os_access.py --output='foo.out' dir1 dir2 dir3
+# Namespace(directories=[u'dir1', u'dir2', u'dir3'], output_file=u'foo.out')
+print(arguments);
+print('output file:', arguments.output_file);# output file: foo.out
+#print('output_file' in arguments); # True
+# directories: [u'dir1', u'dir2', u'dir3']
+print('directories:', arguments.directories);
 	
-	
-	
+print(arguments.__weakref__); # None
+# {'directories': [u'dir1', u'dir2', u'dir3'], 'output_file': u'foo.out'}
+print(arguments.__dict__);
 	
 	
 	
